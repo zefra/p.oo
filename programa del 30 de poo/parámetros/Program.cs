8ff0eeceea5dbe629341dd04f20be4e42af1ede0
zefra <50537557+zefra@users.noreply.github.com>
2019-09-29 20:31:15 -0700

    Add files via upload

---
+++ programa del 30 de poo/parámetros/Program.cs
@@ -0,0 +1,85 @@
+﻿using System;
+
+namespace clientes
+{
+
+    class Persona
+    {
+        public string nombre;
+        public string apellido;
+        public int id;
+        public int cel;
+
+        public string Nombre
+      {
+         get{return nombre;}
+         set{nombre = value;}
+      }
+      public string Apellido
+      {
+         get{ return apellido;}
+         set{ apellido = value;}
+      }
+      public int16 Id
+      {
+         get{ return id;}
+         set{ id = value;}
+      }
+      public int16 Cel
+      {
+         get{ return pais;}
+         set{ pais = value;}
+      }
+      public void imprime()
+
+    {
+        Console.WriteLine("Informacion del Cliente es: {0}", nombre,apellido,id,cel);
+    }
+        public Persona()
+        {
+            nombre = "Lelouch";
+            apellido = "Lamperiu";
+        }
+    }
+
+       `class Anonymous {
+       public static void anonimiza( Persona p)
+    {
+            p.nombre = "xxxxxxxxxxx";
+            p.apellido = "xxxxxxxxxxx";       
+    }
+
+    public static void cambia(ref Persona p )
+      {
+          p = new Persona();
+          p.nombre = "new";
+      }
+}
+
+    class Program
+    {
+        static void duplica( ref int x )
+        {
+            x = x * 2;
+        }    
+
+        static int suma(int x, int y)
+        {
+            return x+y;
+        }    
+        static void suma(int x, int y, out int resultado) 
+        {
+            resultado = x + y;
+        }    
+
+
+        static void Main(string[] args)
+        {
+            int a = 2;
+            int b = 3;
+            int r = suma(a,b);
+            int r2;
+            suma(a,b,out r2);
+
+
+            Console.WriteLine(r2);
