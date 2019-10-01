1cb449f7032839d91fe3914848ded41e949f1499
zefra <50537557+zefra@users.noreply.github.com>
2019-09-30 18:49:13 -0700

    Add files via upload

---
+++ Ejemplo padre poo/padre/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace Padre
+{
+    class Componente
+    {
+        public string nombre;
+        public Contenedor padre;
+
+        public Componente(string nombre, Contenedor padre)
+        {
+            this.nombre = nombre;
+            this.padre = padre;
+        }
+        public void Print(){
+            Console.WriteLine($" Soy (nombre) y mi padre es(padre)");
+        }
+    }
+    class Contenedor
+    {
+        public string nombre;
+        public List<Componente> componentes = new List <Componente>();
+
+        public Contenedor (string nombre)
+        {
+        Componentes.Add(new Componente("Primero"),this);
+        Componentes.Add(new Componente("Segundo"),this);
+        Componentes.Add(new Componente("Tercero"),this);
+        }
+        public void print(){
+            foreach var in Componenetes{
+                hijo.Print();
+            }
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Contenedor Padre1 = new Contenedor("Contenedor 1");
+            Contenedor Padre2 = new Contenedor("Contenedor 2");
+
+            Padre1.Print();
+            Console.WriteLine("-----------------");
+            Padre2.Print();
+        }
+    }
+}
