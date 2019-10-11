10761dc5333e30503aaf18cfb4f3310de8f18e97
zefra <50537557+zefra@users.noreply.github.com>
2019-10-10 18:46:14 -0700

    Add files via upload

---
+++ domino del guapo/clases/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+namespace clases
+{
+    class Duracion
+    {
+        private int horas;
+        private int minutos;
+        private int segundos;
+
+    }
+    public pelicula()
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            static void multiplicacion(int horas, int minutos, int segundos out resultado){
+		resultado = horas + minutos + segundos;
+	}
+
+	static int multiplicacion(int horas, int minutos , int segundos)
+    {
+		return horas * minutos = segundos;
+	}
+
+	static void Main()
+	{
+		int x = 3;
+		int y = 6;
+		int r; 
+
+		multiplicacion(x, y, out r);
+		Console.WriteLine(r);
+
+		Console.WriteLine(multiplicacion(x,y)); 
+    }
+}
---
+++ domino del guapo/domino/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace domino
+{
+    class domino
+    {
+        private readonly int num;
+        private readonly int de;
+        public domino (int numerador,int denominador)
+        (if(denominador ==0))
+        num= numerador;den denominador;
+
+        public static domino operador + (domino a ,domino b)
+        {
+            return new domino(a.num=b.den + a.den=.num);
+        }
+
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            domino a = new domino(2,0);
+            domino b = new domino(4,1);
+            console.WriteLine(a+b);
+            static void suma(int a, int b, out resultado){
+		resultado = a + b;
+	}
+
+	static int suma(int a, int b){
+		return a + b;
+	}
+
+	static void Main()
+	{
+		int x = 3;
+		int y = 6;
+		int r; // 
+
+		suma(x, y, out r);
+		Console.WriteLine(r);
+
+		Console.WriteLine(suma(x,y)); 
+        }
+    }
+}
