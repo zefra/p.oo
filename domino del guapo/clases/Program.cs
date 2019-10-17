27d85269afd3bcc5b4b574dae8729a0a94ff335a
zefra <50537557+zefra@users.noreply.github.com>
2019-10-17 12:34:54 -0700

    Update Program.cs

--- domino del guapo/clases/Program.cs
+++ domino del guapo/clases/Program.cs
@@ -4,20 +4,27 @@ using System.Collections.Generic;
 
 namespace clases
 {
-    class Duracion
+     class Duracion
     {
         private int Horas;
         private int Minutos;
         private int Segundos;
-
-        public void SetHoras(int Horas)
-        {
-            this.Horas = Horas;
-        } 
-        public int GetHoras()
-        {
-            return this.Horas;
-        }
+         
+         public int horas
+      {
+         get{return horas;}
+         set{horas = value;}
+      }
+         public string minutos
+      {
+         get{return minutos;}
+         set{minutos = value;}
+      }
+         public string segundos
+      {
+         get{return segundos;}
+         set{segundos = value;}
+      }
 
         public Duracion(int Horas, int Minutos, int Segundos)
     {
@@ -25,17 +32,35 @@ namespace clases
         this.Minutos = Minutos;
         this.Segundos = Segundos;
     }
+    public void imprime()
+    {
+        int H;
+        int M;
+        int S;
+        H = (Horas*360);
+        M = (Minutos*60);
+        S = Segundos;
+        Console.WriteLine("Horas: {0}\nMinutos: {1}\nSegundos: {2}", H, M, S);
+        Console.WriteLine("Horas en segundos: {0}\n", H);
+    }
     
     public void imprime()
     {
-        Console.WriteLine("Horas: {0}\nMinutos: {1}\nSegundos: {2}\n", this.Horas, this.Minutos, this.Segundos);
+        Console.WriteLine("Horas: {0}\nMinutos: {1}\nSegundos: {2}", this.Horas, this.Minutos, this.Segundos);
     }
     }
     class Program
     {
         static void Main(string[] args)
         {
-            Duracion Pelicula = new Duracion(2,30,0);
-            D.imprime();
+            Duracion Pelicula = new Duracion(2,15,12);
+            Console.WriteLine("Pelicula");
+            Pelicula.imprime();
+            Duracion Cancion = new Duracion(0,2,15);
+            Console.WriteLine("\nCancion");
+            Cancion.imprime();
+            Duracion Partido = new Duracion(2,2,15);
+            Console.WriteLine("\nPartido");
+            Partido.imprime();
     }
 }
