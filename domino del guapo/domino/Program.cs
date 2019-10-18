b25dbfc7c2ee6bb77a7acf2d687dd08586d99f27
zefra <50537557+zefra@users.noreply.github.com>
2019-10-17 21:02:46 -0700

    Update Program.cs

--- domino del guapo/domino/Program.cs
+++ domino del guapo/domino/Program.cs
@@ -4,15 +4,25 @@ namespace domino
 {
     class domino
     {
-        private readonly int num;
+        private readonly int nu;
         private readonly int de;
+	     public int nu
+      {
+         get{return nu;}
+         set{nu = value;}
+      } 
+	     public int de
+      {
+         get{return de;}
+         set{de = value;}
+      }
         public domino (int numerador,int denominador)
         (if(denominador ==0))
         num= numerador;den denominador;
 
         public static domino operador + (domino a ,domino b)
         {
-            return new domino(a.num=b.den + a.den=.num);
+            return new domino(a.nu=b.den + a.den=.nu);
         }
 
     }
