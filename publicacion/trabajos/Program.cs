331db78589679b90ff71d37eeef8bd90f1611fc6
zefra <50537557+zefra@users.noreply.github.com>
2019-10-23 12:52:23 -0700

    Add files via upload

---
+++ publicacion/trabajos/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace publicacion
+{
+    class publicacion
+    {
+        protected string correo;
+        protected string nombre;
+        protected int fecha;
+        protected string mensaje;
+
+        public void textiar()
+        {
+            console.Writeline("mandar mensaje {0}",mensaje);
+            
+        }
+      {
+          public void print mensaje()
+          {
+              console.Writeline(mensaje);
+          }
+      }
+        class Estado : publicacion
+        {
+            public estado
+        }
+           
+    }
+}
