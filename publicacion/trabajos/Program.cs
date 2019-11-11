1655221079e9823dcde43e6f624284d9edbd2013
zefra <50537557+zefra@users.noreply.github.com>
2019-11-10 19:42:01 -0800

    Update Program.cs

--- publicacion/trabajos/Program.cs
+++ publicacion/trabajos/Program.cs
@@ -9,6 +9,27 @@ namespace publicacion
         protected string nombre;
         protected int fecha;
         protected string mensaje;
+        
+        public string Correo
+    {
+        get{return correo;}
+        set{correo = value;}
+    } 
+         public int Fecha
+    {
+        get{return fecha;}
+        set{fecha = value;}
+    } 
+         public string Mensaje
+    {
+        get{return mensaje;}
+        set{mensaje = value;}
+    } 
+         public string Nombre
+    {
+        get{return nombre;}
+        set{nombre = value;}
+    } 
 
         public void textiar()
         {
@@ -23,7 +44,35 @@ namespace publicacion
       }
         class Estado : publicacion
         {
-            public estado
+             Estado público ( cadena  nombre , cadena  mensaje , cadena  fecha , cadena  correo , cadena  l )
+        {
+          
+        }
+        pública  anulación  vacío  de impresión ()
+        {
+              Consola . WriteLine ( " publicado por: {0} mensaje: {1} reacciono a la publicación: {2} comento: {3} link {4} \ n (estado) hoy es un buen dia\ n " , Nombre , Mensaje , fecha , correo );
+        }
+    }
+    clase  Publicacion_fotos : publicacion
+    {
+        público  Publicacion_fotos ( cadena de  nombre , cadena  mensaje , cadena de fecha , cadena  correo  )
+        {
+            
+        }
+        pública  anulación  vacío  de impresión ()
+        {
+            Consola . WriteLine ( " publicado por: {0} foto: {1} reacciono: {2} comento: {3} enlace: {4} \ n , (foto) saliendo de la escuela \ n " , Nombre , Mensaje , amigo , comentario , enlace );
+        }
+    }
+
+     Programa de clase
+    {
+         vacío  estático Main ( string [] args )
+        {
+            var  O  =  nuevo  Estado ( " pedro " , " molesto " , " todo mejora " , " pagina web" );
+            var  P =  nueva  Publicación_fotos ( " daniela fernandes " , " foto" ,  " te mirAS bien en la foto " , " pagina web " );
+            O . print ();
+            P . print ();
         }
            
     }
