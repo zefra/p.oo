8ade08e6154e910c0853ffdc63d1c86eaaa69f8f
zefra <50537557+zefra@users.noreply.github.com>
2019-11-15 23:29:46 -0800

    Update Program.cs

--- publicacion/trabajos/Program.cs
+++ publicacion/trabajos/Program.cs
@@ -48,29 +48,30 @@ namespace publicacion
         {
           
         }
-        pública  anulación  vacío  de impresión ()
+        public  anulación  vacío  de impresión ()
         {
               Consola . WriteLine ( " publicado por: {0} mensaje: {1} reacciono a la publicación: {2} comento: {3} link {4} \ n (estado) hoy es un buen dia\ n " , Nombre , Mensaje , fecha , correo );
         }
     }
-    clase  Publicacion_fotos : publicacion
+    clase 
+        Publicacion_fotos : publicacion
     {
-        público  Publicacion_fotos ( cadena de  nombre , cadena  mensaje , cadena de fecha , cadena  correo  )
+        public  Publicacion_fotos ( cadena de  nombre , cadena  mensaje , cadena de fecha , cadena  correo  )
         {
             
         }
-        pública  anulación  vacío  de impresión ()
+        public  anulación  vacío  de impresión ()
         {
             Consola . WriteLine ( " publicado por: {0} foto: {1} reacciono: {2} comento: {3} enlace: {4} \ n , (foto) saliendo de la escuela \ n " , Nombre , Mensaje , amigo , comentario , enlace );
         }
     }
 
-     Programa de clase
+     Program de clase
     {
          vacío  estático Main ( string [] args )
         {
             var  O  =  nuevo  Estado ( " pedro " , " molesto " , " todo mejora " , " pagina web" );
-            var  P =  nueva  Publicación_fotos ( " daniela fernandes " , " foto" ,  " te mirAS bien en la foto " , " pagina web " );
+            var  P =  nueva  Publicación_fotos ( " daniela fernandes " , " foto" ,  " te miras bien en la foto " , " pagina web " );
             O . print ();
             P . print ();
         }
