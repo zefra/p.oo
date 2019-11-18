b155b477dd3dc1a3d01c15ea7719c46301612527
zefra <50537557+zefra@users.noreply.github.com>
2019-11-18 15:34:43 -0800

    Add files via upload

---
+++ trabajo en clase/error/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace error
+{
+    class Program
+    {
+        static void main()
+        {
+            int valor;
+            console.writeline("ingresa tu edad:");
+            string captura = console. readline();
+            valor=int.parse(captura);
+            console.writeline("en diez anos tendras{0}",valor,100/valor);
+            console.readkey();
+            try
+            {
+                string captura = console.readline();
+                int valor = int.parse(captura);
+                console.writeline(valor)
+            }
+            catch (format exception)
+            {
+                console.writeline("formato incorrecto")
+            }
+            console.readkey();
+        }
+    }
+}
